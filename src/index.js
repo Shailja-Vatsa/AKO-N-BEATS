b35a5da3d3881a20692b492f2097235a2c77d34c
Shailja <vatsashailja@gmail.com>
2026-09-10 14:14:38 +0530

    Add music file handling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const fs = require("fs");
+
 console.log("AKO-N-BEATS");
 console.log("Terminal Music Player");
+
+const songs = fs.readdirSync("./music");
+
+console.log("\nAvailable Songs:");
+
+songs.forEach((song, index) => {
+  console.log(`${index + 1}. ${song.replace(".mp3", "")}`);
+});
